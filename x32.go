64420bfb49e0955d30ed1f99f0483c836d86af2e
AlCutter <al-github@9600.org>
2019-09-23 15:41:54 +0100

    Add support for configuring track mappings

--- x32.go
+++ x32.go
@@ -22,6 +22,13 @@ type Proxy struct {
 	server       *osc.UDPConn
 	reaperClient *osc.UDPConn
 	x32Client    *osc.UDPConn
+
+	mapping mapping
+}
+
+type mapping struct {
+	reaper map[string]string
+	x32    map[string]string
 }
 
 func splitAddress(a string) (string, int, error) {
@@ -31,9 +38,103 @@ func splitAddress(a string) (string, int, error) {
 	}
 	port, err := strconv.Atoi(bits[1])
 	if err != nil {
-		return "", 0, fmt.Errorf("invalid port: %q", err)
+		return "", 0, fmt.Errorf("invalid port: %v", err)
 	}
 	return bits[0], port, nil
+}
+
+func parseRange(r string) (int, int, error) {
+	rng := strings.Split(r, "-")
+	switch l := len(rng); l {
+	case 1:
+		v, err := strconv.Atoi(rng[0])
+		if err != nil {
+			return -1, -1, fmt.Errorf("invalid range %v", err)
+		}
+		return v, v, nil
+	case 2:
+		lo, err := strconv.Atoi(rng[0])
+		if err != nil {
+			return -1, -1, fmt.Errorf("invalid range lower %v", err)
+		}
+		hi, err := strconv.Atoi(rng[1])
+		if err != nil {
+			return -1, -1, fmt.Errorf("invalid range upper %v", err)
+		}
+		if lo > hi {
+			return -1, -1, fmt.Errorf("invalid range %d > %d", hi, lo)
+		}
+
+		return lo, hi, nil
+	default:
+		return -1, -1, fmt.Errorf("invalid range %q", r)
+	}
+}
+
+func isReaperType(m string) error {
+	if m != "track" {
+		return fmt.Errorf("invalid reaper mapping type %s", m)
+	}
+	return nil
+}
+
+var validX32MappingTypes = map[string]bool{
+	"ch":    true,
+	"bus":   true,
+	"dca":   true,
+	"mtx":   true,
+	"main":  true,
+	"auxin": true,
+	"fxrtn": true,
+}
+
+func isX32Type(m string) error {
+	if !validX32MappingTypes[m] {
+		return fmt.Errorf("invalid x32 mapping type %s", m)
+	}
+	return nil
+}
+
+func buildMapping(conf map[string]string) (*mapping, error) {
+	ret := mapping{
+		reaper: make(map[string]string),
+		x32:    make(map[string]string),
+	}
+	for k, v := range conf {
+		reaTrack := strings.Split(k, "/")
+		if err := isReaperType(reaTrack[0]); err != nil {
+			return nil, err
+		}
+		reaLo, reaHi, err := parseRange(reaTrack[1])
+		if err != nil {
+			return nil, err
+		}
+		x32Track := strings.Split(v, "/")
+		if err := isX32Type(x32Track[0]); err != nil {
+			return nil, err
+		}
+		x32Lo, x32Hi, err := parseRange(x32Track[1])
+		if err != nil {
+			return nil, err
+		}
+		if reaSpan, x32Span := reaHi-reaLo+1, x32Hi-x32Lo+1; reaSpan != x32Span {
+			return nil, fmt.Errorf("invalid mapping - reaper range %d != x32 range %d", reaSpan, x32Span)
+		}
+
+		for reaN, x32N := reaLo, x32Lo; reaN <= reaHi; reaN, x32N = reaN+1, x32N+1 {
+			reaK := fmt.Sprintf("%s/%d", reaTrack[0], reaN)
+			x32K := fmt.Sprintf("%s/%d", x32Track[0], x32N)
+			if _, ok := ret.reaper[reaK]; ok {
+				return nil, fmt.Errorf("duplicate mapping for %s", reaK)
+			}
+			if _, ok := ret.x32[x32K]; ok {
+				return nil, fmt.Errorf("duplicate mapping for %s", x32K)
+			}
+			ret.reaper[reaK] = x32K
+			ret.x32[x32K] = reaK
+		}
+	}
+	return &ret, nil
 }
 
 func NewProxy(config ProxyConfig) (*Proxy, error) {
@@ -63,13 +164,18 @@ func NewProxy(config ProxyConfig) (*Proxy, error) {
 		return nil, fmt.Errorf("couldn't create reaper UDP connection: %q", err)
 	}
 
-	lConn.SetExactMatch(false)
-
+	mapping, err := buildMapping(config.Mapping)
+	if err != nil {
+		return nil, err
+	}
 	p := &Proxy{
 		server:       lConn,
 		reaperClient: rConn,
 		x32Client:    xConn,
+		mapping:      *mapping,
 	}
+
+	glog.Infof("Mapping:\n%v", *mapping)
 
 	return p, nil
 }
---
+++ x32_test.go
@@ -0,0 +1,146 @@
+package x32
+
+import (
+	"testing"
+)
+
+func TestSplitAddress(t *testing.T) {
+	for _, test := range []struct {
+		desc     string
+		address  string
+		wantHost string
+		wantPort int
+		wantErr  bool
+	}{
+		{
+			desc:     "Valid",
+			address:  "host:1234",
+			wantHost: "host",
+			wantPort: 1234,
+		}, {
+			desc:    "InvalidPort",
+			address: "host:banana",
+			wantErr: true,
+		}, {
+			desc:     "NoHost",
+			address:  ":4567",
+			wantHost: "",
+			wantPort: 4567,
+		},
+	} {
+		t.Run(test.desc, func(t *testing.T) {
+			gotHost, gotPort, gotErr := splitAddress(test.address)
+			if test.wantErr && gotErr != nil {
+				return
+			}
+			if test.wantErr && gotErr == nil {
+				t.Fatal("got no error, wanted error")
+			}
+			if gotErr != nil {
+				t.Fatalf("got %v, want no error", gotErr)
+			}
+			if gotHost != test.wantHost {
+				t.Errorf("got host %s, want %s", gotHost, test.wantHost)
+			}
+			if gotPort != test.wantPort {
+				t.Errorf("got port %d, want %d", gotPort, test.wantPort)
+			}
+		})
+	}
+}
+
+func TestParrseRange(t *testing.T) {
+	for _, test := range []struct {
+		desc    string
+		r       string
+		wantErr bool
+		wantLo  int
+		wantHi  int
+	}{
+		{
+			desc:   "ValidSingle",
+			r:      "56",
+			wantLo: 56,
+			wantHi: 56,
+		}, {
+			desc:   "ValidRange",
+			r:      "23-45",
+			wantLo: 23,
+			wantHi: 45,
+		}, {
+			desc:    "InvalidReversed",
+			r:       "6-2",
+			wantErr: true,
+		}, {
+			desc:    "InvalidIncomplete",
+			r:       "6-",
+			wantErr: true,
+		}, {
+			desc:    "InvalidGarbage",
+			r:       "Hello!",
+			wantErr: true,
+		},
+	} {
+		t.Run(test.desc, func(t *testing.T) {
+			gotLo, gotHi, gotErr := parseRange(test.r)
+			if test.wantErr && gotErr != nil {
+				return
+			}
+			if test.wantErr && gotErr == nil {
+				t.Fatalf("Got no error, wanted error")
+			}
+			if gotErr != nil {
+				t.Fatalf("Got unexpected error %s", gotErr)
+			}
+			if gotLo != test.wantLo || gotHi != test.wantHi {
+				t.Fatalf("Got values %d,%d want %d,%d", gotLo, gotHi, test.wantLo, test.wantHi)
+			}
+		})
+	}
+}
+
+func TestIsReaperType(t *testing.T) {
+	if gotErr := isReaperType("track"); gotErr != nil {
+		t.Fatalf("got unexpected error %s, want no error", gotErr)
+	}
+
+	if gotErr := isReaperType("ch"); gotErr == nil {
+		t.Fatal("got no error, want error")
+	}
+}
+
+func TestIsX32Type(t *testing.T) {
+	for _, test := range []struct {
+		t       string
+		wantErr bool
+	}{
+		{
+			t: "ch",
+		}, {
+			t: "bus",
+		}, {
+			t: "dca",
+		}, {
+			t: "mtx",
+		}, {
+			t: "main",
+		}, {
+			t: "auxin",
+		}, {
+			t: "fxrtn",
+		}, {
+			t:       "bananas",
+			wantErr: true,
+		},
+	} {
+		t.Run(test.t, func(t *testing.T) {
+			gotErr := isX32Type(test.t)
+			if test.wantErr && gotErr != nil {
+				return
+			}
+			if gotErr != nil {
+				t.Fatalf("Got unexpected error %s, want no error", gotErr)
+			}
+		})
+	}
+}
